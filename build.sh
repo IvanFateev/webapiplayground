83a78c3eb4bb85f0f2b861c5108088bc165b8cb9
JohnPoison <johnny.j.poison@gmail.com>
2017-03-13 18:49:03 +0300

    slightly changed configuration

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker build -t dotnet-build-image -f Dockerfile.build .
 docker rm mydotnet
-docker run --name "mydotnet" -it -p 5000:5000 dotnet-build-image ./run.sh
+docker run --name "mydotnet" -p 80:5000 -it dotnet-build-image
